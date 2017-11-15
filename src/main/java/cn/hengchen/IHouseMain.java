c0d10319424bbaaa279aefa1ffade580028649de
chengfang1023 <foreveringcc@163.com>
2017-11-15 13:48:03 +0800

    主入口执行

--- src/main/java/cn/hengchen/IHouseMain.java
+++ src/main/java/cn/hengchen/IHouseMain.java
@@ -1,5 +1,11 @@
 package cn.hengchen;
 
-public class IHouseMain {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class IHouseMain {
+	public static void main(String[] args) {
+		SpringApplication.run(IHouseMain.class,args);
+	}
 }	
